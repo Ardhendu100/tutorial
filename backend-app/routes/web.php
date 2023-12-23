64616038ec4ce92e8a74c8bbabff3e421f9e5fbd
ardhendu.sahoo <ardhendu.sahoo@alumnetworks.com>
2023-12-24 00:03:23 +0530

    phone paye integration

--- backend-app/routes/web.php
+++ backend-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('phonepe',[PaymentController::class,'phonePe']);
+Route::any('phonepe-response',[PaymentController::class,'response'])->name('response');
